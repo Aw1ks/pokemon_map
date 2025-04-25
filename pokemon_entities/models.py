e9c0b479c41567313f8f3cf3047570087f76eb50
Aw1ks <aw1ksss331@gmail.com>
2025-04-25 14:52:00 +0300

    verbose_name has been added and required fields have been defined

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,39 +1,40 @@
-from django.db import models  # noqa F401
+from django.db import models
 from django.utils import timezone
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200, blank=True)
-    title_en = models.CharField(max_length=200, blank=True)
-    title_jp = models.CharField(max_length=200, blank=True)
+    """Информация о покемоне."""
 
-    image = models.ImageField(blank=True)
-    description = models.TextField(default='Описание в разработке')
+    title = models.CharField('Название (рус.)', max_length=200)
+    title_en = models.CharField('Название (англ.)', max_length=200)
+    title_jp = models.CharField('Название (яп.)', max_length=200)
+    image = models.ImageField('Изображение')
+    description = models.TextField('Описание', default='Описание в разработке')
 
     previous_evolution = models.ForeignKey(
         'self',
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        related_name='previous_evolutions'
+        related_name='previous_evolutions',
+        verbose_name='Предыдущая эволюция'
     )    
 
     def __str__(self):
         return f'{self.title}'
 
-
 class PokemonEntity(models.Model):
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+    """Информация о местоположении покемона."""
 
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    latitude = models.FloatField('Широта')
+    longitude = models.FloatField('Долгота')
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='Покемон')
 
-    appeared_at = models.DateTimeField(default=timezone.now)
-    disappeared_at = models.DateTimeField(default=timezone.now)
+    appeared_at = models.DateTimeField('Время появления', default=timezone.now)
+    disappeared_at = models.DateTimeField('Время появления', default=timezone.now)
 
-    level = models.IntegerField(blank=True, null=True)   
-    health = models.IntegerField(blank=True, null=True)  
-    attack = models.IntegerField(blank=True, null=True)  
-    protection = models.IntegerField(blank=True, null=True) 
-    stamina = models.IntegerField(blank=True, null=True)
-
+    level = models.IntegerField('Уровень', blank=True, null=True)   
+    health = models.IntegerField('Здоровье', blank=True, null=True)  
+    attack = models.IntegerField('Атака', blank=True, null=True)  
+    protection = models.IntegerField('Защита', blank=True, null=True) 
+    stamina = models.IntegerField('Выносливость', blank=True, null=True)
